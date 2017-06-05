c9e17dfa99bbc34598c49b309d71988f5bfdbad9
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-06-05 14:49:11 -0700

    Added data and small update to timeseries code

--- R/rhessys_output_timeseries.R
+++ R/rhessys_output_timeseries.R
@@ -8,14 +8,13 @@ ui <- fluidPage(
   headerPanel('RHESSys Basin Output'),
   sidebarPanel(
     selectInput('var1', 'Variable 1', names(happy$bd), selected = names(happy$bd)[19]),
-    sliderInput(inputId = "date_range", 
-                label = "Choose Date Range:", 
-                value = c(happy$bd$date[1], max = happy$bd$date[length(happy$bd$date)]),
-                min = happy$bd$date[1], max = happy$bd$date[length(happy$bd$date)]),
-    
-    #    dateRangeInput("date_range", "Please select date range", 
-#                   start=happy$bd$date[1],
-#                   end=happy$bd$date[length(happy$bd$date)]),
+    sliderInput(
+      inputId = "date_range", 
+      label = "Choose Date Range:", 
+      value = c(happy$bd$date[1], max = happy$bd$date[length(happy$bd$date)]),
+      min = happy$bd$date[1], 
+      max = happy$bd$date[length(happy$bd$date)]
+      ),
     radioButtons("ylog", "Log Y Axis?", c("Log" = "y", "No log" = ""))
   ),
   mainPanel(
@@ -25,7 +24,7 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   selectedData <- reactive({
-    happy$bd[, c("date", input$var1)]
+    happy$bd[, c("date", input$var1)]     # Subset date column and column of interest
   })
 
   output$plot1 <- renderPlot({
